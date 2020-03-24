80c2a1b000b1d31b1c374833a26c9701a33d693b
Jonathan OBrien <jonathan.r.obrien@gmail.com>
2020-03-24 19:40:10 -0400

    Add bootstrap
    Add bootstrap framework for styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PatientComponent } from './patient/patient.component';
 import { EncounterComponent } from './encounter/encounter.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PatientComponent,
-    EncounterComponent
+    EncounterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
