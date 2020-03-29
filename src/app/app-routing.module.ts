04a6174a8606358ff4ec9f31adc60a4d41efe41f
Jonathan OBrien <jonathan.r.obrien@gmail.com>
2020-03-29 19:41:33 -0400

    Add routing
    Add basic routing to application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { PatientComponent } from './patient/patient.component';
+import { EncounterComponent } from './encounter/encounter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  component: HomeComponent },
+  { path: 'patient/:id',  component: PatientComponent },
+  { path: 'encounter/:id',  component: EncounterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { PatientComponent } from './patient/patient.component';
 import { EncounterComponent } from './encounter/encounter.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    NavbarComponent,
     PatientComponent,
-    EncounterComponent,
-    NavbarComponent
+    EncounterComponent
   ],
   imports: [
     BrowserModule,
